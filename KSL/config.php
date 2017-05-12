3d497fbc28abaac4d691360c30d09dfa1b42b713
Jakub <jakub.tusan@ness.com>
2017-05-12 08:22:23 +0200

    Main config example

---
+++ KSL/config.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+set_time_limit(1);
+ini_set('memory_limit', '12M');
+
+define('TIMEZONE', 'Europe/Bratislava');
+date_default_timezone_set(TIMEZONE);
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['driver']         = 'mysql';
+$config['db']['host']           = 'localhost';
+$config['db']['username']       = 'ksl_test';
+$config['db']['password']       = 'ksl_test';
+$config['db']['database']       = 'ksl_test';
+$config['db']['collation']      = 'utf8_general_ci';
+$config['db']['charset']        = 'utf8';
+$config['db']['port']           = 3306;
+$config['db']['timezone']       = '+02:00';
