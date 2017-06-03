581c6db1f04b22f443609cbe4a5b6f8eb3c1455e
Jakub <jakub.tusan@ness.com>
2017-06-03 18:51:21 +0200

    Checking for admin permissions on admin pages

--- KSL/Controllers/BaseAdmin.php
+++ KSL/Controllers/BaseAdmin.php
@@ -3,5 +3,14 @@ namespace KSL\Controllers;
 
 class BaseAdmin extends Base
 {
- 
+    public function __construct() {
+        $user       = \KSL\Models\User::GetUser();
+        $user_id    = $user ? $user->id : null;
+
+        if(\KSL\Models\UserPermissions::hasPermission($user_id, 'admin') === false) {
+            die('Nemáte oprávnenie na prístup k nasledujúcej stránke.');
+        }
+
+        return call_user_func_array(array('parent', '__construct'), func_get_args());
+    }
 }
