dcb423f5ed1a92c2794d9c50f819d5eaee3a369c
Jakub Tušan <jakub.tusan@masternaut.com>
2019-02-01 22:47:29 +0100

    fix menu opening on xs screens

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -42,6 +42,16 @@ DropDown.propTypes = {
 }
 
 export default class MainMenu extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = { opened: false };
+        this.toggleOnClick = this.toggleOnClick.bind(this);
+    }
+
+    toggleOnClick() {
+        this.setState({ opened: !this.state.opened });
+    }
+
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -49,11 +59,11 @@ export default class MainMenu extends PureComponent {
                     <Link to="/" className="navbar-brand">
                         <img src="/images/logo_mini.png" width="77" height="47" alt="KSL.sk logo"/>
                     </Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" onClick={this.toggleOnClick} data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className={"collapse navbar-collapse" + (this.state.opened ? " show" : "")} id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active"><Link to="/" className="nav-link">Úvod</Link></li>
                             <li className="nav-item"><Link to="/schedule" className="nav-link">Rozpis</Link></li>
