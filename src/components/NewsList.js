c34d8133952ffb1879de9b65eeac17192b85b939
Mjuhas <juhasmiroslav945@gmail.com>
2019-02-20 20:34:24 +0100

    closed #9 #5 #10

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,17 @@
 import React, { PureComponent } from 'react';
 
+
 export default class NewsList extends PureComponent {
     render() {
-        return <div>Novinky</div>;
+        return <div>
+                    <div class="bg-light py-4"  >
+                        <div class=" text-white h5 bg-warning  mx-4   py-3 px-4">
+                                    Žiadne ďalšie zápasy nie sú naplánované
+                        </div>
+                    </div>
+                    <div class="my-3 mx-5 border-bottom" >
+                        <p class="display-2 font-weight-light">Novinky</p>
+                    </div> 
+                </div>;
     }
 }
--- src/components/UpcomingMatches.js
+++ src/components/UpcomingMatches.js
@@ -1,7 +1,12 @@
 import React, { PureComponent } from 'react';
 
+
 export default class UpcomingMatches extends PureComponent {
     render() {
-        return <div>Žiadne dalsie zápasy</div>;
+            return  <div class="bg-light">
+                        <div class=" text-secondary border-bottom mx-5 py-3 ">
+                                    <h2 class="font-weight-normal">Najbližšie zápasy</h2>
+                        </div>
+                    </div>;
     }
 }
