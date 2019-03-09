c03622c41df12e2cb673bacb7d1204c85a6781a9
mzachar6 <mzachar6@gmail.com>
2019-03-09 15:46:05 +0100

    src/components/Playground.js

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,12 +1,15 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import providers from '../dataProvider';
 
-export default class Playground extends PureComponent {
-    render(){
-      return(
-        <li className="list-group-item list-group-item-action list-group-item-light">
-            <a href={this.props.httpLink} > {this.props.street} </a>
-            <span className="float-right"> {this.props.province} </span>
-        </li>
-      );
-    }
-}
+const playgrounds = providers.getPlaygroundsList();
+const playgroundListItem = playgrounds.map( (playground,i) =>
+
+    <li className="list-group-item list-group-item-action list-group-item-light" key={playground.id}>
+    <a href='' > {playground.name} </a>
+    <span className="float-right"> {playground.district} </span>
+    </li>
+
+);
+
+export default playgroundListItem;
+
