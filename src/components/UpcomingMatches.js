64c9e902eb4a0fb8a080e1edb2628ae8e3045fe7
Jakub Tušan <jakub.tusan@masternaut.com>
2019-03-19 20:02:23 +0100

    fixed upcommingmatches and newsList

--- src/components/UpcomingMatches.js
+++ src/components/UpcomingMatches.js
@@ -3,10 +3,17 @@ import React, { PureComponent } from 'react';
 
 export default class UpcomingMatches extends PureComponent {
     render() {
-            return  <div class="bg-light">
-                        <div class=" text-secondary border-bottom mx-5 py-3 ">
-                                    <h2 class="font-weight-normal">Najbližšie zápasy</h2>
+            return <div>
+                     <div className="bg-light">
+                        <div className=" text-secondary border-bottom mx-5 py-3 ">
+                                    <h2 className="font-weight-normal">Najbližšie zápasy</h2>
                         </div>
-                    </div>;
+                    </div>
+                    <div className="bg-light py-4"  >
+                        <div className=" text-white h5 bg-warning  mx-4   py-3 px-4">
+                             Žiadne ďalšie zápasy nie sú naplánované
+                         </div>
+                    </div>
+                </div>;
     }
 }
