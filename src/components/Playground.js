d8d88920f8e635ecbb915fdb8c65204807c7cbb0
mzachar6 <mzachar6@gmail.com>
2019-03-31 17:24:29 +0200

    Playground component added

---
+++ src/components/Playground.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+export default class Playground extends PureComponent {
+    render() {
+        return (
+            <li className="list-group-item list-group-item-action list-group-item-light" key={this.props.id}>
+                <a href='' > {this.props.name} </a>
+                <span className="float-right"> {this.props.district} </span>
+            </li>
+        );
+    }
+}
+
