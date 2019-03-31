54e0c32a0ef2e296b337e52f9043b910ea299cf1
mzachar6 <mzachar6@gmail.com>
2019-03-31 17:44:01 +0200

    temporary href link added

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -4,7 +4,7 @@ export default class Playground extends PureComponent {
     render() {
         return (
             <li className="list-group-item list-group-item-action list-group-item-light" key={this.props.id}>
-                <a href='' > {this.props.name} </a>
+                <a href='#playground' > {this.props.name} </a>
                 <span className="float-right"> {this.props.district} </span>
             </li>
         );
