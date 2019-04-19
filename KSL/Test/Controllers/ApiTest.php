d7d3954841cdcfe7637deba578460b70fcc602fe
Jakub Tušan <jakub.tusan@masternaut.com>
2019-04-20 01:55:50 +0200

    graphQl unit test for teams query

--- KSL/Test/Controllers/ApiTest.php
+++ KSL/Test/Controllers/ApiTest.php
@@ -22,17 +22,20 @@ class ApiTest extends TestBaseControllers {
         $this->assertSame("Syntax Error: Unexpected <EOF>", $result->errors[0]->message);
     }
 
-    public function testTeamsQuery() {     
+    public function testTeamsQuery() {
         $response       = $this->getRouteResponse('/api.php', 'POST', [
-            'query' => '{ teams { name } }'
+            'query' => '{ teams { id, name, short, captain { id } } }'
         ]);
         $body = $response->getBody();
         $body->seek(0);
         
         $result = json_decode($body->read(1024));
-        //var_dump($result);
-        //die("TESTEE2");
-        $this->assertSame("Syntax Error: Unexpected <EOF>", $result->errors[0]->message);
+        $teams = $result->data->teams;
+
+        $this->assertCount(3, $teams);
+        $this->assertTrue($this->compareObjects(['id' => 1, 'name' => 'Bulls', 'short' => 'BUL', 'captain' => [ 'id' => 1 ] ], $teams[0]));
+        $this->assertTrue($this->compareObjects(['id' => 2, 'name' => 'Team A', 'short' => 'TEA', 'captain' => [ 'id' => 4 ] ], $teams[1]));
+        $this->assertTrue($this->compareObjects(['id' => 3, 'name' => 'Team X', 'short' => 'X', 'captain' => null ], $teams[2]));
     }
 
 }
