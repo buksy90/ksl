46e454e6d1dd7bd2e1d79897efc8225cabf40a5b
Jakub Tušan <jakub.tusan@masternaut.com>
2019-04-26 20:42:34 +0200

    Updated teams query

--- KSL/Test/Controllers/ApiTest.php
+++ KSL/Test/Controllers/ApiTest.php
@@ -38,4 +38,38 @@ class ApiTest extends TestBaseControllers {
         $this->assertTrue($this->compareObjects(['id' => 3, 'name' => 'Team X', 'short' => 'X', 'captain' => null ], $teams[2]));
     }
 
+    public function testTeamsQueryStatistics() {
+        $response       = $this->getRouteResponse('/api.php', 'POST', [
+            'query' => '{ teams { standing, score, games_played, games_won, games_lost } }'
+        ]);
+        $body = $response->getBody();
+        $body->seek(0);
+        
+        $result = json_decode($body->read(1024));
+        //var_dump($result);
+        $teams = $result->data->teams;
+
+        $this->assertCount(3, $teams);
+        $this->assertTrue($this->compareObjects(['standing' => 1, 'score' => "11:2", 'games_played' => 1, 'games_won' => 1, 'games_lost' => 0], $teams[0]));
+        $this->assertTrue($this->compareObjects(['standing' => 2, 'score' => "2:11", 'games_played' => 1, 'games_won' => 0, 'games_lost' => 1], $teams[1]));
+        $this->assertTrue($this->compareObjects(['standing' => 3, 'score' => "0:0", 'games_played' => 0, 'games_won' => 0, 'games_lost' => 0], $teams[2]));
+    }
+
+    public function testTeamsQueryStatistics2() {
+        $response       = $this->getRouteResponse('/api.php', 'POST', [
+            'query' => '{ teams { points, points_scored, points_allowed, success_rate } }'
+        ]);
+        $body = $response->getBody();
+        $body->seek(0);
+        
+        $result = json_decode($body->read(1024));
+        //var_dump($result);
+        $teams = $result->data->teams;
+
+        $this->assertCount(3, $teams);
+        $this->assertTrue($this->compareObjects(['points' => 3, 'points_scored' => 11, 'points_allowed' => 2, 'success_rate' => 100], $teams[0]));
+        $this->assertTrue($this->compareObjects(['points' => 0, 'points_scored' => 2, 'points_allowed' => 11, 'success_rate' => 0], $teams[1]));
+        $this->assertTrue($this->compareObjects(['points' => 0, 'points_scored' => 0, 'points_allowed' => 0, 'success_rate' => 0], $teams[2]));
+    }
+
 }
