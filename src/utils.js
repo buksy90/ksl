aa84c3e4cc4ae9eb288969e869c3bef4d2929c54
mzachar6 <mzachar6@gmail.com>
2019-05-17 11:22:46 +0200

    create new util function getPlayTime()

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const Utils = {
 
-  getDateOption: function( date ){
+  getDateOption: function (date) {
     const days = ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota'];
     const actualDate = new Date(date);
     const getDay = actualDate.getDay();
@@ -8,6 +8,18 @@ const Utils = {
     const getMonth = actualDate.getMonth() + 1;
 
     return `${getDate}.${getMonth} ${days[getDay]}`;
+  },
+
+  getPlayTime: function (time) {
+    const currentDate = new Date(time);
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+
+    if (minutes < 10) {
+      return `${hours}:0${minutes}`
+    }
+    
+    return `${hours}:${minutes}`;
   }
 
 }
