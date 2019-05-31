6a774666e805808fe4f026a0019070c481802137
Mjuhas <juhasmiroslav945@gmail.com>
2019-05-31 16:49:21 +0200

    Display list of news on index page

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -7,22 +7,26 @@ export default class NewsList extends PureComponent {
         
         this.state = { list: [] };
 
-        providers.getNewsList().then(data => {
+      providers.getNewsList().then(data => {
           this.setState({ list: data.news });
-        });
+      });
     }
 
     render() {
         return <div className="container">
                     <div className="border-bottom mt-2">
                         <h1 className="font-weight-light">Novinky</h1>
-                    </div> 
-                <ul className="list-group list-group-flush">
-                  { this.state.list.map( (news,row) => {
-                        return( <td key={row}>{news.id} {news.id} {news.title} {news.id} {news.date} </td>)
-                    })
-                  }
-                </ul>
-                </div>;
+                    </div>  
+                  {this.state.list.map((news,index) =>{
+                    return(
+                          <div className="card mt-4"  key={index}>
+                              <h3 className="card-header font-italic mt-0">{news.title}</h3>
+                              <div className="card-body"> 
+                                <div className="card-text text-justify">{news.text}</div>
+                            </div>
+                          </div>
+                        )
+                    })}                
+                 </div>
     }
 }
