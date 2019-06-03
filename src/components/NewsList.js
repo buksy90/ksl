9a87f255678e3cd5d2d3ea34659d1d53ace89e1f
MJuhas <47060773+MJuhas@users.noreply.github.com>
2019-06-03 20:42:15 +0200

    Get news list (#47)
    * I have problem with onload data - news from dataProvider
    
    * Display list of news on index page
    
    * resolve conflict
    
    * Set botton margin

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,12 +1,32 @@
 import React, { PureComponent } from 'react';
-
+import providers from '../dataProvider';
 
 export default class NewsList extends PureComponent {
+    constructor(props){
+        super(props);
+        
+        this.state = { list: [] };
+
+      providers.getNewsList().then(data => {
+          this.setState({ list: data.news });
+      });
+    }
+
     render() {
-        return <div className="container">
+        return <div className="container mb-4">
                     <div className="border-bottom mt-2">
                         <h1 className="font-weight-light">Novinky</h1>
-                    </div> 
-                </div>;
+                    </div>  
+                  {this.state.list.map((news,index) =>{
+                    return(
+                          <div className="card mt-4"  key={index}>
+                              <h5 className="card-header font-italic mt-0">{news.title}</h5>
+                              <div className="card-body"> 
+                                <div className="card-text text-justify">{news.text}</div>
+                            </div>
+                          </div>
+                        )
+                    })}                
+                 </div>
     }
 }
