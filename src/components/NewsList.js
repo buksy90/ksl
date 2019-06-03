4a20cc10cf127c1d928c22aa875035f8e9e8e4a3
Mjuhas <juhasmiroslav945@gmail.com>
2019-06-03 20:36:48 +0200

    Set botton margin

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -11,16 +11,16 @@ export default class NewsList extends PureComponent {
           this.setState({ list: data.news });
       });
     }
-    
+
     render() {
-        return <div className="container">
+        return <div className="container mb-4">
                     <div className="border-bottom mt-2">
                         <h1 className="font-weight-light">Novinky</h1>
                     </div>  
                   {this.state.list.map((news,index) =>{
                     return(
                           <div className="card mt-4"  key={index}>
-                              <h3 className="card-header font-italic mt-0">{news.title}</h3>
+                              <h5 className="card-header font-italic mt-0">{news.title}</h5>
                               <div className="card-body"> 
                                 <div className="card-text text-justify">{news.text}</div>
                             </div>
